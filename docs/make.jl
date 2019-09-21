b4ad0082777c83eb19902667bd8f94d132951852
Steve Kelly <kd2cca@gmail.com>
2019-09-21 18:00:21 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -13,7 +13,4 @@ makedocs(
 
 deploydocs(
     repo = "github.com/JuliaGeometry/Meshing.jl.git",
-    devbranch = "master",
-    devurl = "dev",
-    versions = ["stable" => "v^", "v#.#", devurl => devurl],
 )
