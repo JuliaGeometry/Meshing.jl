383a5ae05a98e1c83dba4365da97058e62be4333
Steve Kelly <kd2cca@gmail.com>
2024-07-12 11:49:52 +0000

    sentinal

--- src/Meshing.jl
+++ src/Meshing.jl
@@ -6,6 +6,8 @@ include("marching_tetrahedra.jl")
 include("marching_cubes.jl")
 include("isosurface.jl")
 
+is_test_registry() = true
+
 export isosurface,
        MarchingCubes,
        MarchingTetrahedra
